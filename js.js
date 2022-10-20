fa2ee750ce4f538c70004a0c4500465cb88d7ca4
AVolosciuc <anea.tanas@mail.ru>
2022-10-20 20:31:15 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,45 @@
+// const name = 1;
+// let name1 = "string";
+// let name2 = true;
+// let name3 = false;
+// let name4 = null;
+// let name5 = undefined;
+
+// console.log(name1);
+// var name2 = 3;
+
+// name = "salut";
+// name1 = "salut";
+
+
+// console.log(name);
+// console.log(name1);
+
+// let score = null;
+
+// let ion = "2";
+// let vasea = 4;
+
+// score = ion + vasea
+// console.log(score);
+
+// console.log(typeof ion);
+
+// const nume = "Volosciuc"
+// const prenume = "Ana"
+
+// console.log(nume + " " + prenume);
+
+
+// let a = "ana";
+// let b = 5;
+// console.log(a + " " + b);
+
+// alert ("hello")
+
+
+// prompt ("what is your name")
+
+let number = 5;
+number += 10;
+console.log(number);
