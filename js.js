ddcc5f88215855b54c9c131b8e9af3149be73d2d
AVolosciuc <anea.tanas@mail.ru>
2022-11-01 19:36:49 +0200

    first com

--- js.js
+++ js.js
@@ -40,6 +40,107 @@
 
 // prompt ("what is your name")
 
-let number = 5;
-number += 10;
-console.log(number);
+// let number = 5;
+// number += 10;
+// console.log(number);
+
+
+// const a = 2;
+// const b = 3;
+// if (a > b) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+// if (a > 10) {
+//     console.log("incorect");
+// } else if (a === "2") {
+//     console.log("sunt egale");
+// } else {
+//     console.log("Error"); 
+// }
+
+// const c = 10;
+// const e = 20;
+
+
+// if (c === 10 || e === "20") {
+//     console.log("sunt egale");
+// } else {
+//     console.log(" nu sunt egale");
+// }
+
+
+// let fruit = "apple";
+//     fruit =  "orange";
+// switch (fruit) {
+//     case "orange":
+//             console.log("orange $0.59");
+//         break;
+//     case "apple":
+//             console.log("apple $1.59");
+//         break;
+//     case "banana":
+//             console.log("banana $2.59");
+//         break;
+//     default:
+//         break;
+// }
+
+
+// let i = 1;
+
+// while (i > 0) {
+//     console.log(i);
+//     i += 1;
+// }
+
+// let i = 1;
+
+// do {
+//     console.log(i);
+//     i += 1;
+// } while (i <= 5);
+
+
+// for (let i = 0; i <= 5; i++) {
+//    console.log(i);
+    
+// }
+
+// let array = [
+//     "patru",
+//     "unu",
+//     "doi",
+//     "trei",
+//     "patru",
+//     "cinci"
+// ];
+
+// array.forEach(el => {
+//     if (el.includes ("patru")) {
+//         console.log(el);
+//     }
+// });
+
+
+const first_number = prompt("First number");
+const operator = prompt("mathematical operator");
+const second_number = prompt("Second number");
+
+if (operator === "+") {
+    console.log(Number(first_number) + Number(second_number));
+} else if (operator === "-"){
+    console.log(Number(first_number) - Number(second_number));
+}else if (operator === "*"){
+    console.log(Number(first_number) * Number(second_number));
+}else if (operator === "/"){
+    console.log(Number(first_number) / Number(second_number));
+}else if (operator === "**"){
+    console.log(Number(first_number) ** Number(second_number));
+}else {
+    alert("eeeeerrrrrrrrrrooooooooooor")
+}
+
+
